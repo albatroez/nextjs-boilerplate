c64818984cb5e90493099ecdee3b417075f17f4f
Jakub Zieliński <kuba.s.zielinski@gmail.com>
2024-11-30 18:00:28 +0100

    chore: add prettier and format files

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+
+const config = {
+    tabWidth: 4,
+    trailingComma: "es5",
+    experimentalTernaries: true,
+};
+
+export default config;
